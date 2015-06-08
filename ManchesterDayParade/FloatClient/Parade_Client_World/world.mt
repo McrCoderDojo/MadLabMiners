d23d5a4e278a2ed2e504aab2dd618815ea1955f7
Dave Potts <dave@goopot.co.uk>
2015-06-08 11:45:29 +0100

    Changing to use James' parade as a submodule

--- ManchesterDayParade/FloatClient/Parade_Client_World/world.mt
+++
@@ -1,4 +0,0 @@
-gameid = mcr_coderdojo
-backend = sqlite3
-load_mod_parade_local = true
-load_mod_parade_server = false
--- ManchesterDayParade/FloatClient/mcr_coderdojo/mods/Jeija-minetest-mod-mesecons-d19e975/mesecons/legacy.lua
+++
@@ -1,7 +0,0 @@
-function mesecon:receptor_on(pos, rules)
-	mesecon.receptor_on(pos, rules)
-end
-
-function mesecon:receptor_off(pos, rules)
-	mesecon.receptor_off(pos, rules)
-end
